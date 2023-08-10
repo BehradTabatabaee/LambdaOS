c478c47d156e077ff024a0cf0a736b2f9ddb6372
Behrad Tabatabaee <behradtabatabaee3@gmail.com>
2023-08-10 07:22:56 -0400

    finished bootloader (still too half assed to be called a real Bootloader)

--- Makefile
+++ Makefile
@@ -2,3 +2,4 @@ all:
 	nasm -f bin ./boot/boot.asm -o ./boot/boot.bin
 	dd if=./boot/message.txt >> ./boot/boot.bin
 	dd if=/dev/zero bs=512 count=1 >> ./boot/boot.bin
+	qemu-system-x86_64 -hda ./boot/boot.bin
--- boot/boot.asm
+++ boot/boot.asm
@@ -1,6 +1,8 @@
 ORG 0
 BITS 16
- 
+
+; gotta fill the first 36 bytes so that the bios wont overwrite them for us
+
 _start:
     jmp short start
     nop
@@ -13,6 +15,8 @@ start:
 real_start:
     cli ; clear interrupts
     
+    ; setting all of the segments to 0x7c0 cuz thats the location bios expects our bootloader to be
+
     mov eax,0x7c0
     mov es,eax
     mov ds,eax
@@ -21,18 +25,35 @@ real_start:
     mov sp,0x7c00
 
     sti ; set interrupts
+
+    ; reading from hard drive
+
+    mov ah,2 ; read
+    mov al,1 ; one sector to read
+    mov ch,0 ; cylinder number
+    mov cl,2 ; sector number
+    mov dh,0 ; head number
+    mov bx,buffer ; put the data inside the bx register
+    int 0x13 ; bios interrupt for reading from hard disk
+    jc error ; jump error if the carry flag is set
+    mov si,buffer ; move the buffer to si register so that we can print our message
+    call print
+
+    jmp $
+
+error:
+    mov si,error_message
     call print
     jmp $
 
 print_char:
     mov ah,0x0e
-    mov bh,0x00
-    mov bl,0x07
     int 0x10
     ret
 
 print:
-    mov si, message
+    mov bh,0x00
+    mov bl,0x07
 
 .loop:
     lodsb
@@ -44,7 +65,9 @@ print:
 .done:
     ret
 
-message: db 'Booted Successfully!',0
+error_message: db 'Failed To Read',0
 
 times 510-($-$$) db 0
 dw 0xAA55
+
+buffer:
